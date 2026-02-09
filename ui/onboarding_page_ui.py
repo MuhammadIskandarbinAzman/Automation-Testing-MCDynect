5d2c093ba729085add57a64480a476171d3c5e31
Muhammad <iskandarazman96@gmail.com>
2026-02-09 16:35:19 +0800

    chore: remove duplicate nested repository and merge unique files

---
+++ ui/onboarding_page_ui.py
@@ -0,0 +1,13 @@
+"""
+This module defines UI locators for the On-boarding Page.
+"""
+class OnboardingPageUI:
+    """
+    Locators for elements on the on-boarding page.
+    """
+    # The "Open [Module]" button/link that leads to the dashboard
+    # Works for "Open Licensee", "Open Area Manager", etc.
+    OPEN_MODULE_BUTTON = "xpath=//*[contains(text(), 'Open ')]"
+    
+    # Header to verify we are on the on-boarding page
+    ONBOARDING_HEADER = "h1:has-text('Welcome to Your On Boarding')"
